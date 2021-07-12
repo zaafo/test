552e45ef8ef464172bd8c793991cfbb16b0ba478
zaafo <87318018+zaafo@users.noreply.github.com>
2021-07-12 17:18:59 +0200

    Create commonJenkinsfileFunction_OP.groovy

---
+++ commonJenkinsfileFunction_OP.groovy
@@ -0,0 +1,9 @@
+#!groovy
+// Lint de la chart
+@Deprecated
+def lintHelm(String chartPath) {
+    def workspace = pwd()
+    def completeChartPath = workspace + chartPath
+    sh "cd ${completeChartPath} &&  ls -ltr "
+    sh "cd ${completeChartPath} && helm lint "
+}
